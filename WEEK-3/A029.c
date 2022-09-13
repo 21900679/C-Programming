5a9aab1736b0e220e14c5f277ba0835e4ce0cf15
21900679 <61596743+21900679@users.noreply.github.com>
2022-09-14 00:01:03 +0900

    C 실습

---
+++ WEEK-3/A029.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) {
+	int num1, num2;   // 입력받은 두 수
+	int i;            // 반복문을 위한 변수
+
+  printf("2개의 숫자를 입력 ");
+  scanf("%d %d", &num1, &num2); 
+
+  for(i = 1; i <= 100; i++) {
+    if(i % num1 == 0 || i % num2 == 0){
+      if(i % num2 == 0 && i % num1 == 0){
+      }
+      else
+        printf("%d\n", i);
+    }
+  }
+	
+	return 0;
+}
