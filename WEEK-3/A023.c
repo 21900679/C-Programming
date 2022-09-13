5ecc4e39bdb6e1998dadbf09bfb6717d6103066c
21900679 <61596743+21900679@users.noreply.github.com>
2022-09-13 23:56:51 +0900

    C 실습

---
+++ WEEK-3/A023.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void) {
+	int width, height; // 가로크기, 세로크기
+	int count1 = 0; // "정사각형"의 개수
+	int count2 = 0; // "가로형 직사각형"의 개수 
+	int count3 = 0; // "세로형 직사각형"의 개수
+
+  for(int i = 0; ; i++){
+    printf("직사각형의 가로 크기와 세로 크기를 입력하시오. : ");
+    scanf("%d %d", &width, &height);
+    if(width <= 0 || height <= 0)
+      break;
+    if(width == height)
+      count1 = count1 + 1;
+    else if(width > height)
+      count2 = count2 + 1;
+    else if(width < height)
+      count3 = count3 + 1;
+  }
+
+  printf("\"정사각형\"의 개수는 %d 입니다.\n", count1);
+  printf("\"가로형 직사각형\"의 개수는 %d 입니다.\n", count2);
+  printf("\"세로형 직사각형\"의 개수는 %d 입니다.", count3);
+	
+	return 0;
+}
